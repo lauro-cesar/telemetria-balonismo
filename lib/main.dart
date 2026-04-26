459317073a73fd2db6589f9856ce1a6cc01d6f72
Lauro C Oliveira <lauro@hostcert.com.br>
2026-04-25 22:06:04 -0300

    Preparando para Build

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,5 @@
-import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:geolocator/geolocator.dart';
-import 'package:path/path.dart' as p;
-import 'package:sqflite/sqflite.dart';
-import 'package:uuid/uuid.dart';
-import 'package:web_socket_channel/io.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
 import 'telemetria/gps_tracker_app.dart';
 
 void main() {
